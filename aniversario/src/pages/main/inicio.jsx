396b0c1ccc6965aff9af8b1224c2905f26bd0f85
PC Luciano Di Pietro <huesodipietro@gmail.com>
2026-04-09 16:22:08 -0300

    terminado

--- aniversario/src/pages/main/inicio.jsx
+++ aniversario/src/pages/main/inicio.jsx
@@ -16,22 +16,53 @@ const Aniversario = () => {
       audio: '/audio/de_cabeza.mp3' // Puedes repetir el audio si quieres que dure 2 slides
     },
     {
-      imagen: '/fotos/san_pedro.jpg',
-      titulo: 'Nuestro primer viaje',
-      parrafo: 'Sin tanta planificacion, nos pegamos una hermosa escapada a San Pedro, y si ya habia sospechas de lo nuestro... Pilar termino de confirmarlas al regreso.',
-      audio: '/audio/de_cabeza.mp3' // Puedes repetir el audio si quieres que dure 2 slides
+      imagen: '/fotos/parque.jpeg',
+      titulo: 'Volvés mágico lo cotidiano',
+      parrafo: 'En un parque o en un crucero, tomando mates o cerveza, siempre sos mi mejor compañía.',
+      audio: '/audio/hay_un_lugar_cortado.mp3' // Puedes repetir el audio si quieres que dure 2 slides
     },
     {
-      imagen: '/fotos/san_pedro.jpg',
-      titulo: 'Nuestro primer viaje',
-      parrafo: 'Sin tanta planificacion, nos pegamos una hermosa escapada a San Pedro, y si ya habia sospechas de lo nuestro... Pilar termino de confirmarlas al regreso.',
-      audio: '/audio/de_cabeza.mp3' // Puedes repetir el audio si quieres que dure 2 slides
+      imagen: '/fotos/noche_mar.jpeg',
+      titulo: 'Mi Sol en la oscuridad',
+      parrafo: 'Cuando todo parece perdido, oscuro y sin rumbo, siempre estas vos para sacarme a flote y hacerme sonreír.',
+      audio: '/audio/hay_un_lugar_cortado.mp3' // Puedes repetir el audio si quieres que dure 2 slides
     },
     {
-      imagen: '/fotos/foto3.jpg',
+      imagen: '/fotos/rebelde.jpeg',
+      titulo: 'Bonita de más',
+      parrafo: 'El brillo de tus ojos esa noche fue bello. Amo verte feliz y compartir momentos así con vos.',
+      audio: '/audio/bonita.mp3'
+    },
+    {
+      imagen: '/fotos/sola.jpeg',
+      titulo: 'Simplemente vos',
+      parrafo: 'Una vos solita porque sos la linda de la relación y me gusta mucho esa foto',
+      audio: '/audio/bonita.mp3'
+    },
+       {
+      imagen: '/fotos/playa.jpeg',
+      titulo: 'Viajes y el CEF',
+      parrafo: 'Siempre pienso que nuestras vidas de alguna forma estaban conectadas. No sé, de alguna manera estabamos destinados a ser la pareja ideal',
+      audio: '/audio/y_asi_sera.mp3'
+    },
+      {
+      imagen: '/fotos/uruguay.jpg',
+      titulo: 'Uruguay',
+      parrafo: 'Nosotros haciendonos los lindos luego de caminar 754 km para llegar a una playa',
+      audio: '/audio/y_asi_sera.mp3'
+    },
+     
+    {
+      imagen: '/fotos/teen.png',
+      titulo: 'Reencuentro de los Teen',
+      parrafo: 'Esta  fotito viene del futuro, del dia en que los teen se reencuentren y vayamos al concierto',
+      audio: '/audio/pensar_en_vos.mp3'
+    },
+       {
+      imagen: '/fotos/atardecer.jpg',
       titulo: '¡Felices 2 Años!',
-      parrafo: 'Gracias por enseñarme lo lindo que es el amor sano. Por muchos años más creciendo, riendo y soñando juntos. ¡Te amo infinito!',
-      audio: '/audio/hay_un_lugar.mp3'
+      parrafo: 'Por muchos años más creciendo, riendo y soñando juntos. ¡Te amo infinito! PD: es mi foto favorita de los dos',
+      audio: '/audio/pensar_en_vos.mp3'
     }
   ];
 
@@ -41,7 +72,7 @@ const Aniversario = () => {
   const [terminoDeEscribir, setTerminoDeEscribir] = useState(false);
   
   const audioRef = useRef(new Audio());
-  const velocidadEscritura = 60;
+  const velocidadEscritura = 80;
 
   // Lógica para cambiar y reproducir el audio cuando cambia la diapositiva
   useEffect(() => {
@@ -51,10 +82,13 @@ const Aniversario = () => {
       // Solo cambiamos el audio si es distinto al que ya está sonando
       if (audioRef.current.src !== window.location.origin + audioPath) {
         audioRef.current.pause();
-        audioRef.current.volume = 0.08
+        audioRef.current.volume = 0.1
         audioRef.current.src = audioPath;
         audioRef.current.loop = true; // Para que el pedazo de canción no se corte si ella lee lento
         audioRef.current.play().catch(e => console.log("Error al reproducir:", e));
+        if(audioPath == '/audio/hay_un_lugar_cortado.mp3'){
+          audioRef.current.volume = 0.4
+        }
       }
     }
   }, [indiceActual, comenzado]);
@@ -98,6 +132,7 @@ const Aniversario = () => {
         </div>
       ) : (
         <div className="carrusel">
+        
           <img 
             key={indiceActual} 
             src={diapositivas[indiceActual].imagen} 
@@ -112,10 +147,10 @@ const Aniversario = () => {
               {!terminoDeEscribir && <span className="cursor-titilante">|</span>}
             </p>
             
-            {/* El botón aparece con una animación suave solo cuando el texto termina */}
+              {/* El botón aparece con una animación suave solo cuando el texto termina */}
             {terminoDeEscribir && (
               <button className="btn-siguiente fade-in" onClick={irASiguiente}>
-                {indiceActual === diapositivas.length - 1 ? "Nuestro futuro..." : "Continuar nuestra historia"}
+                {indiceActual === diapositivas.length - 1 ? "Nuestro futuro..." : "Continuar..."}
               </button>
             )}
           </div>
